9a06369732b9f4b2afedf62f6898880df89941a5
te nagusami <tenagusami@hotmail.com>
2022-04-02 15:54:37 +0900

    feat: deformation test

--- Assets/Body.cs
+++ Assets/Body.cs
@@ -1,5 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
+// using System.Collections;
+// using System.Collections.Generic;
 using UnityEngine;
 
 public class Body : MonoBehaviour
@@ -7,12 +7,12 @@ public class Body : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.transform.position = new(0.0f, 0.0f, 0.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(0.0f, 0.0f, 3.0f);
     }
 }
