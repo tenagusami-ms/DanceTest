6f3eb67edcdfb78a4f53f55ac004fb50be8cda6c
te nagusami <tenagusami@hotmail.com>
2022-04-09 15:41:00 +0900

    feat: rotation is added.

--- Assets/Body.cs
+++ Assets/Body.cs
@@ -9,6 +9,7 @@ public class Body : MonoBehaviour
     private float _positionPhase = 0.0f;
     private float _positionAmplitude = 0.02f;
     private float _positionDPhase = 0.1f;
+    private float _rotationAmplitude = 0.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +21,7 @@ public class Body : MonoBehaviour
     void Update()
     {
         transform.position += new Vector3((float) (this._positionAmplitude * Math.Cos(this._positionPhase)),0.0f);
+        transform.Rotate(0.0f, 0.0f, (float) (this._rotationAmplitude * Math.Cos(this._positionPhase)));
         this._positionPhase += this._positionDPhase;
     }
 }
